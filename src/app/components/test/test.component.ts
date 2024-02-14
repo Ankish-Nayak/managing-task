ff571e93e83cd1f3f7c06113c4570a0b3084fab8
ankish2000nayak@gmail.com <ankish2000nayak@gmail.com>
2024-02-14 13:30:39 +0530

    chk-3

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,12 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FrameComponent } from '../../sharedComponents/containers/frame/frame.component';
+import { ChatComponent } from '../chat/chat.component';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [CommonModule, FrameComponent],
+  imports: [CommonModule, FrameComponent, ChatComponent],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss',
 })
-export class TestComponent {}
+export class TestComponent {
+  showChat: boolean = true;
+  onCloseFrame() {
+    this.showChat = false;
+  }
+  openFrame() {
+    this.showChat = true;
+  }
+}
